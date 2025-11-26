24f639bee2e656e84e7d7a310d7a0dac644b2d97
Tkquinn <150734474+Tkquinn@users.noreply.github.com>
2025-11-25 19:38:48 -0500

    added animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,20 +3,44 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -250.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Get the input direction: -1, 0, 1
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	#flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	#play animations
+	
+	#animations if I have time to make a jump animation.
+	#if is_on_floor():
+		#if direction == 0:
+			#animated_sprite.play("Idle")
+		#else:
+			#animated_sprite.play("Move Horizontal")
+	#else:
+		#animated_sprite.play("Jump")
+	
+	if direction == 0:
+		animated_sprite.play("Idle")
+	else:
+		animated_sprite.play("Move Horizontal")
+	
+	#apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
